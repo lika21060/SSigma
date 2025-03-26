d9241f1ff100fe9e6b4bbb5e819cce709139462c
lika21060 <likaasanidze000@gmail.com>
2025-03-26 21:16:13 +0400

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -6,24 +6,38 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'phone_number', 'first_name', 'last_name']
-
+        fields = ['id', 'username', 'email', 'phone_number', 'first_name', 'last_name']
+        
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
     
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'phone_number', 'first_name', 'last_name', 'password', 'password2']
+        fields = ['email', 'username', 'phone_number', 'password', 'password2', 'first_name', 'last_name']
         
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({'password': 'again!'})
+            raise serializers.ValidationError9({'password': "Passwords don't match"})
         return attrs
     
     def create(self, validated_data):
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
-        
+    
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+        def update(self, instance, validated_data):
+            password = validated_data.pop('password', None)
+            if password:
+                instance.set_password(password)
+            return super().update(instance, validated_data)
+    
