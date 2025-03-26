99c3e0fcf97a56b53015e2ae092255d9b6a3a311
lika21060 <likaasanidze000@gmail.com>
2025-03-26 21:15:52 +0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,15 +1,32 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
-from rest_framework.viewsets import GenericViewSet
-from users.models import User
-from users.serializers import UserSerializer, RegisterSerializer
+from rest_framework import mixins, viewsets
+from .models import User
+from .serializers import UserSerializer, RegisterSerializer, UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
+from products.permissions import IsObjectOwnerReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-class UserViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
     
-class CreateUserViewSet(CreateModelMixin,GenericViewSet):
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-    
+
+class ProfileViewSet(viewsets.GenericViewSet, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated, IsObjectOwnerReadOnly]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
+    def perform_destroy(self, instance):
+        instance.delete()
